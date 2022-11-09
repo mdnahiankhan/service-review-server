2e4b5191ca094f78e813937a8246e6f6da9c1eec
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-11-09 19:20:19 +0600

    create connection for mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -14,7 +14,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.kzjjck3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kzjjck3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
